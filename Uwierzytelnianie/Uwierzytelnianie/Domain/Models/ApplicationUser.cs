6682791eb1e2ba05915c72fe12aa4ea00c0b1026
Ola-M <56175718+Ola-M@users.noreply.github.com>
2021-04-18 15:30:21 +0200

    CreatedProject

---
+++ Uwierzytelnianie/Uwierzytelnianie/Domain/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uwierzytelnianie.Domain.Models
+{
+    public class ApplicationUser
+    {
+    }
+}
